ff29a3566285ec4c8f92b299b1a164817ffa7544
Egor Moiseenko <kuzy000@gmail.com>
2016-02-08 00:47:43 +0300

    Add config files parsing & Fix command line parsing

--- Configuration.h
+++ Configuration.h
@@ -10,42 +10,19 @@ namespace RunFragment {
 namespace po = boost::program_options;
 
 struct Configuration {
-	enum class Format { GlslSandbox, ShaderToy };
-
-	boost::optional<std::string> filename;
+	boost::optional<std::string> file;
 	float time;
 
-	Format format;
-	struct Uniform {
-		std::string time;
-
-		static getDefault(std::string name, Format format) {
-		}
-	} uniform;
-
-	static Configuration fromVariablesMap(const po::variables_map& vm) {
-		auto lookupStr = [&vm] (std::string name, std::string defvalue) {
-			if(vm.count(name)) {
-				return vm[name].as<std::string>();
-			}
-			else {
-				return defvalue;
-			}
-		};
-
-		Configuration result;
-		result.filename = vm.count("file") ? boost::optional<std::string> {vm["file"].as<std::string>()}
-		                                   : boost::none;
-		result.time = vm.count("time") ? vm["time"].as<float>() : 1.0f;
-		result.format = lookupStr("format", "s") == "s" ? Format::ShaderToy
-		              : lookupStr("format", "s") == "g" ? Format::GlslSandbox
-		              : throw std::runtime_error("unvalid --format argument");
-
-		result.uniform.time = lookupStr("utime", Format::ShaderToy)
-
-		return result;
-	}
+	std::string iResolution;
+	std::string iGlobalTime;
+	std::string iGlobalDelta;
+	std::string iGlobalFrame;
+	std::string iChannelTime;
+	std::string iMouse;
+	std::string iDate;
+	std::string iSampleRate;
+	std::string iChannelResolution;
+	std::string iChanneli;
 };
 
 }
-
--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <stdexcept>
+#include <fstream>
 
 #include <boost/program_options/options_description.hpp>
 #include <boost/program_options/parsers.hpp>
@@ -9,19 +10,62 @@
 #include "GlfwWindow.h"
 #include "Configuration.h"
 
+
+const std::string shaderToy = 
+R"raw(
+time = 1
+
+iResolution = iResolution
+iGlobalTime = iGlobalTime
+iGlobalDelta = iGlobalDelta
+iGlobalFrame = iGlobalFrame
+iChannelTime = iChannelTime
+iMouse = iMouse
+iDate = iDate
+iSampleRate = iSampleRate
+iChannelResolution = iChannelResolution
+iChanneli = iChanneli
+)raw";
+
+const std::string glslSandbox =  R"raw(
+time = 1
+
+iResolution = resolution
+iGlobalTime = time
+iGlobalDelta = iGlobalDelta
+iGlobalFrame = iGlobalFrame
+iChannelTime = iChannelTime
+iMouse = mouse
+iDate = iDate
+iSampleRate = iSampleRate
+iChannelResolution = iChannelResolution
+iChanneli = iChanneli
+)raw";
+
+
 namespace po = boost::program_options;
 
 int main(int argc, char* argv[]) {
-	po::options_description gen {"General options"};
+	po::options_description gen;
 	gen.add_options()
-		("file",   po::value<std::string>(), "Input file")
-		("time,t", po::value<float>(),       "Set value that multiplies the time")
-		("help,h",                           "Display help message");
+		("file",     po::value<std::string>(), "Input file")
+		("config,c", po::value<std::string>(), "Config file")
+		("time,t",   po::value<float>(),       "Set value that multiplies the time")
+		("help,h",                             "Display help message")
+		("format,f", po::value<std::string>(), "Format of uniforms:\n    g : GLSLSandbox\n    s : ShaderToy (default)");
 
-	po::options_description uni {"Uniform options"};
+	po::options_description uni;
 	uni.add_options()
-		("format,f", po::value<std::string>(), "Format of uniforms:\n    g : GLSLSandbox\n    s : ShaderToy (default)")
-		("utime",    po::value<std::string>(), "Set name of time uniform");
+		("iResolution",        po::value<std::string>(), "Set name of iResolution uniform")
+		("iGlobalTime",        po::value<std::string>(), "Set name of iGlobalTime uniform")
+		("iGlobalDelta",       po::value<std::string>(), "Set name of iGlobalDelta uniform")
+		("iGlobalFrame",       po::value<std::string>(), "Set name of iGlobalFrame uniform")
+		("iChannelTime",       po::value<std::string>(), "Set name of iChannelTime uniform")
+		("iMouse",             po::value<std::string>(), "Set name of iMouse uniform")
+		("iDate",              po::value<std::string>(), "Set name of iDate uniform")
+		("iSampleRate",        po::value<std::string>(), "Set name of iSampleRate uniform")
+		("iChannelResolution", po::value<std::string>(), "Set name of iChannelResolution uniform")
+		("iChanneli",          po::value<std::string>(), "Set name of iChanneli uniform");
 
 	po::options_description desc;
 	desc.add(gen).add(uni);
@@ -33,21 +77,71 @@ int main(int argc, char* argv[]) {
 	auto parser = po::command_line_parser(argc, argv).options(desc).positional(pos);
 	try {
 		po::store(parser.run(), vm);
-		po::notify(vm);
 	}
 	catch(std::exception& e) {
 		std::cerr << e.what() << std::endl;
 
 		return EXIT_FAILURE;
 	}
-
+	
 	if(vm.count("help")) {
-		std::cout << "Usage: run_fragment [options] file" << std::endl << std::endl;
+		std::cout << "Usage: run_fragment [options] file" << std::endl;
 		std::cout << desc << std::endl;
 
 		return EXIT_SUCCESS;
 	}
+	
+	if(vm.count("config")) {
+		std::ifstream iniFile {vm["config"].as<std::string>()};
+		
+		try {
+			po::store(po::parse_config_file(iniFile, desc, true), vm);  
+		}
+		catch(std::exception& e) {
+			std::cerr << e.what() << std::endl;
+	
+			return EXIT_FAILURE;
+		}
+	}
+	
+	if(vm.count("format")) {
+		auto arg = vm["format"].as<std::string>();
+		if(arg == "s") {
+			std::istringstream ss {shaderToy};
+			po::store(po::parse_config_file(ss, desc, true), vm);
+		}
+		else if(arg == "g") {
+			std::istringstream ss {glslSandbox};
+			po::store(po::parse_config_file(ss, desc, true), vm);
+		}
+		else {
+			std::cerr << "invalid argument of '--format'" << std::endl;
 
+			return EXIT_FAILURE;
+		}
+	}
+	else {
+		std::istringstream ss {shaderToy};
+		po::store(po::parse_config_file(ss, desc, true), vm);
+	}
+	
+	RunFragment::Configuration config;
+	
+	config.file = vm.count("file") 
+	            ? boost::optional<std::string> {vm["file"].as<std::string>()} 
+	            : boost::none;
+	config.time = vm["time"].as<float>();
+
+	config.iResolution = vm["iResolution"].as<std::string>();
+	config.iGlobalTime = vm["iGlobalTime"].as<std::string>();
+	config.iGlobalDelta = vm["iGlobalDelta"].as<std::string>();
+	config.iGlobalFrame = vm["iGlobalFrame"].as<std::string>();
+	config.iChannelTime = vm["iChannelTime"].as<std::string>();
+	config.iMouse = vm["iMouse"].as<std::string>();
+	config.iDate = vm["iDate"].as<std::string>();
+	config.iSampleRate = vm["iSampleRate"].as<std::string>();
+	config.iChannelResolution = vm["iChannelResolution"].as<std::string>();
+	config.iChanneli = vm["iChanneli"].as<std::string>();
 
 	return 0;
 
