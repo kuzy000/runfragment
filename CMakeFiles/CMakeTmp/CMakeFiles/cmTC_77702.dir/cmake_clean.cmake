03ade8b53fec2a25a56d26d1b4a9e6fa27afb8a7
Egor Moiseenko <kuzy000@gmail.com>
2016-08-20 11:11:40 +0300

    Rework project organization

---
+++ CMakeFiles/CMakeTmp/CMakeFiles/cmTC_77702.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cmTC_77702.dir/feature_tests.c.o"
+  "cmTC_77702.pdb"
+  "cmTC_77702"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/cmTC_77702.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
